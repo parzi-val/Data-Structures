064865ec979791b8db6a9adb98a7a1df7560d056
KR Bala <86968472+parzi-val@users.noreply.github.com>
2023-10-10 23:53:57 +0530

    Create HeteroListsExample.java

---
+++ Examples/HeteroListsExample.java
@@ -0,0 +1,19 @@
+import Lists.node;
+
+class Main {
+    public static void main(String[] args) {
+        node<?> list = new node<>("hello");
+
+        list.append("world"); // String values
+        list.append(42); // Any kinds of numeric values
+        list.append(new Integer[] { 1, 2, 3, 4 }); // Arrays (of any type)
+
+        node<?> sublist = new node<>("nested");
+        sublist.append("linked");
+        sublist.append("list");
+        sublist.append(new String[] { "hello", "world" });
+
+        list.append(sublist); // even other linked lists
+        list.printList();
+    }
+}
