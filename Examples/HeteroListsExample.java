055939c7bae20700a6e5b20483134658ca9d5edb
KR Bala <krbala1511@gmail.com>
2024-04-29 22:47:52 +0530

    fixed constructors

--- Examples/HeteroListsExample.java
+++ Examples/HeteroListsExample.java
@@ -2,13 +2,13 @@ import Lists.node;
 
 class Main {
     public static void main(String[] args) {
-        node<?> list = new node<>("hello");
+        node<?> list = new node<>();
 
         list.append("world"); // String values
         list.append(42); // Any kinds of numeric values
         list.append(new Integer[] { 1, 2, 3, 4 }); // Arrays (of any type)
 
-        node<?> sublist = new node<>("nested");
+        node<?> sublist = new node<>();
         sublist.append("linked");
         sublist.append("list");
         sublist.append(new String[] { "hello", "world" });
